f6d46fa39be5e742bbaf9410478cfcdfa5a2648d
gadgeski <xtwh.ce5bil@gmail.com>
2025-05-08 00:04:49 +0900

    Created index.html and more

---
+++ script.js
@@ -0,0 +1,20 @@
+// 全ての画像要素取得
+const images = document.querySelectorAll(".marquee-inner-stop img");
+const marqueeInners = document.querySelectorAll(".marquee-inner-stop");
+
+// 各画像にホバーイベント追加
+images.forEach((img) => {
+  img.addEventListenner("mouseenter", () => {
+    // 全ての.marquee-inner-stopのアニメーション停止
+    marqueeInners.forEach((marqueeInner) => {
+      marqueeInner.classList.add("marquee-paused");
+    });
+  });
+
+  img.addEventListener("mouseleave", () => {
+    // 全ての.marquee-inner-stopのアニメーション再開
+    marqueeInner.forEach((marqueeInner) => {
+      marqueeInner.classList.remove("marquee-paused");
+    });
+  });
+});
